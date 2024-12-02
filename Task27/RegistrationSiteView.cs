0259e63286f374dbb44a0d2596ba947bcd30372f
Thomas2424 <113831322+Tomas-618@users.noreply.github.com>
2024-12-02 19:26:59 +0400

    Initial commit

---
+++ Task27/RegistrationSiteView.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Task27
+{
+    public partial class RegistrationSiteView : Form
+    {
+        public RegistrationSiteView()
+        {
+            InitializeComponent();
+        }
+    }
+}
